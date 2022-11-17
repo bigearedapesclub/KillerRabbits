001be16ca9cd6f8020bd763c106d716fa44ea2fb
PlushPets <96663878+PlushPets@users.noreply.github.com>
2022-11-17 10:09:18 +0700

    New KillerRabbits

---
+++ precache-manifest.0ace1fa30306d07ac9af3d2b40c99d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c05e89a70f4a142d4e",
+    "url": "/static/css/main.444ad052.chunk.css"
+  },
+  {
+    "revision": "29c05e89a70f4a142d4e",
+    "url": "/static/js/main.c7cd7822.chunk.js"
+  },
+  {
+    "revision": "89cbdf1418fb73c99b71",
+    "url": "/static/js/runtime~main.9667c5b8.js"
+  },
+  {
+    "revision": "50cf6d7496b25d143102",
+    "url": "/static/js/2.9a4fabd2.chunk.js"
+  },
+  {
+    "revision": "94d78f9f36a1be11b0ef",
+    "url": "/static/js/3.2f2d2a6e.chunk.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "1f6ba16b40634026f46122772d3f280a",
+    "url": "/index.html"
+  }
+];
